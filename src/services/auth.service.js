90a725ce73db590117dcbec06e40d40431243811
arvapu <arvydas.apulskis123@gmail.com>
2020-11-05 21:36:13 +0200

    introduced env file

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,23 +3,22 @@ import { create } from 'pkce';
 import SessionService from './session.service'
 
 class AuthService {
-  constructor() {
-    this.clientId = '91c7849b-db97-46d8-8275-0330e1a46a97';
-    this.authorizationUri = 'http://localhost:8000/authorize';
-    this.tokenUri = 'http://localhost:8000/api/token';
-    this.redirectUri = 'http://localhost:8080/auth/callback';
-  }
+
+  static AUTHORIZATION_URI = process.env.VUE_APP_AUTH_SERVER_BASE_URL + '/authorize' ;
+  static TOKEN_URI         = process.env.VUE_APP_AUTH_SERVER_BASE_URL + '/api/token';
+  static REDIRECT_URI      = process.env.VUE_APP_BASE_URL + '/auth/callback';
+  static CLIENT_ID         = process.env.VUE_APP_CLIENT_ID;
 
   async login() {
     const codePair = create();
     const state = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    
-    const authUrl = new URL(this.authorizationUri);
+
+    const authUrl = new URL(AuthService.AUTHORIZATION_URI);
 
     authUrl.search = new URLSearchParams({
       response_type: 'code',
-      client_id: this.clientId,
-      redirect_uri: this.redirectUri,
+      client_id: AuthService.CLIENT_ID,
+      redirect_uri: AuthService.REDIRECT_URI,
       state: state,
       code_challenge: codePair.codeChallenge,
       code_challenge_method: 'S256'
@@ -31,6 +30,8 @@ class AuthService {
   }
 
   async getToken() {
+    console.log(AuthService.AUTHORIZATION_URI, AuthService.TOKEN_URI, AuthService.REDIRECT_URI, AuthService.CLIENT_ID);
+
     const params = new URLSearchParams(window.location.search);
     
     if(!params.has('code') && !params.has('state')) {
@@ -47,10 +48,10 @@ class AuthService {
 
     try {
 
-      const response = await axios.post(this.tokenUri, {
+      const response = await axios.post(AuthService.TOKEN_URI, {
         grant_type: 'authorization_code',
-        client_id: this.clientId,
-        redirect_uri: this.redirectUri,
+        client_id: AuthService.CLIENT_ID,
+        redirect_uri: AuthService.REDIRECT_URI,
         code: code,
         state: state,
         code_verifier: codeVerifier
