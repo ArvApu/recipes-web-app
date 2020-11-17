591ff96c2fc59c48f81df2a1a0b6a98e7d001e3c
arvapu <arvydas.apulskis123@gmail.com>
2020-11-17 22:49:31 +0200

    added header content and footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <header id="header"> LINKS HERE  </header>
+
+    <div id="content">
+      CONTENT HERE
+      <!-- <router-view/> -->
+    </div>
+
+    <footer id="footer"> META INFO HERE  </footer>
   </div>
 </template>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
+  @import './assets/css/main.css';
+  /* Can add style here or in main.css file */
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,8 +9,6 @@
     <br>
     {{ this.user }}
 
-    SOME FAKING TEXT
-
   </div>
 </template>
 
