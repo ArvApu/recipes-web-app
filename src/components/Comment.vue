f27955224ea6327ff1703808e0ea15449bb8ef72
arvapu <arvydas.apulskis123@gmail.com>
2020-11-26 14:10:24 +0200

    comments added validation errors display

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,6 +1,9 @@
 <template>
   <div class='comment-create'>
         <form id="comment-form" @submit.prevent="handle">
+
+           <Alerts :errors="errors"/>
+
             <p class='form-group'>
                 <label for="title"> Title </label>
                 <input class='form-input' type="text" id="title" name="title" v-model="data.title" requried>
@@ -20,12 +23,16 @@
 </template>
 
 <script>
+import Alerts from './Alerts.vue'
 import ApiFactory from '@/api/api.factory'
 
 const commentsApi = ApiFactory.get('comments');
 
 export default {
   name: 'Comment',
+  components: {
+      Alerts
+  },
   props: {
     recipeUserId: Number,
     recipeId: Number,
@@ -39,7 +46,8 @@ export default {
         title: this.title ? this.title : null,
         comment: this.comment ? this.comment : null,
       },
-      shouldEdit: this.commentId ? true : false
+      shouldEdit: this.commentId ? true : false,
+      errors: [],
     };
   },
   methods: {
@@ -51,13 +59,31 @@ export default {
         }
     },
     store() {
-        commentsApi.create(this.recipeUserId, this.recipeId, this.data);
-        this.$emit('created');
+        commentsApi.create(this.recipeUserId, this.recipeId, this.data)
+        .then(() => {
+            this.$emit('created');
+            this.onSuccess();
+        })
+        .catch((errors) => {
+            this.onError(errors);
+        });
     },
     edit() {
-        commentsApi.update(this.recipeUserId, this.recipeId, this.commentId, this.data);
-        this.$emit('updated');
+        commentsApi.update(this.recipeUserId, this.recipeId, this.commentId, this.data)
+        .then(() => {
+            this.$emit('created');
+            this.onSuccess();
+        })
+        .catch((errors) => {
+            this.onError(errors);
+        });
     },
+    onSuccess() {
+        this.errors = [];
+    },
+    onError(errors) {
+        this.errors = errors;
+    }
   }
 }
 </script>
